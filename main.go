df50d942578f20682318d1f9b4327e30124bf4e2
Przemek Delewski <przemyslaw_delewski@o2.pl>
2024-04-02 17:26:27 +0200

    Extracting json utils

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/elastic/go-elasticsearch/v8/typedapi/types"
-	"strconv"
 )
 
 const (
@@ -39,105 +38,6 @@ const queryContent = `{
 func printColoredTextWithBackground(text, textColor, backgroundColor string) {
 	// ANSI escape code for clearing the line and setting background color
 	fmt.Printf("\033[K%s%s%s%s%s\n", backgroundColor, textColor, text, Reset, Reset)
-}
-
-func printJSONHelper(data map[string]interface{}, indent string, currentPath []string, matchPath []string, matcher func([]string, []string) bool) {
-	for key, value := range data {
-		switch v := value.(type) {
-		case map[string]interface{}:
-			currentPath = append(currentPath, key)
-			if matcher(currentPath, matchPath) {
-				fmt.Println(Red+indent, key, ": {"+Reset)
-			} else {
-				fmt.Println(indent, key, ": {")
-			}
-			printJSONHelper(v, indent+"    ", currentPath, matchPath, matcher)
-			currentPath = currentPath[:len(currentPath)-1]
-			fmt.Println(indent, " }")
-		case []interface{}:
-			currentPath = append(currentPath, key)
-			if matcher(currentPath, matchPath) {
-				fmt.Println(Red+indent, key, ": ["+Reset)
-			} else {
-				fmt.Println(indent, key, ": [")
-			}
-
-			currentPath = currentPath[:len(currentPath)-1]
-			for i, item := range v {
-				if nested, ok := item.(map[string]interface{}); ok {
-					fmt.Println(indent + "    " + " {")
-					currentPath = append(currentPath, key)
-					currentPath = append(currentPath, strconv.Itoa(i))
-					printJSONHelper(nested, indent+"        ", currentPath, matchPath, matcher)
-					currentPath = currentPath[:len(currentPath)-1]
-					currentPath = currentPath[:len(currentPath)-1]
-					fmt.Println(indent + "    " + " },")
-				} else {
-					fmt.Println(indent+"    ", item)
-				}
-			}
-			fmt.Println(indent, " ]")
-		default:
-			currentPath = append(currentPath, key)
-			if matcher(currentPath, matchPath) {
-				fmt.Println(Red+indent, key, ":", value, Reset)
-			} else {
-				fmt.Println(indent, key, ":", value)
-			}
-			currentPath = currentPath[:len(currentPath)-1]
-		}
-	}
-}
-
-func isSubSlice(slice1, slice2 []string) bool {
-	// If the second slice is empty, it's considered a sub-slice
-	if len(slice2) == 0 {
-		return true
-	}
-
-	// Iterate over both slices
-	for i := 0; i < len(slice2) && i < len(slice1); i++ {
-		// If the elements at corresponding indices don't match, return false
-		if slice2[i] == "*" {
-			continue
-		}
-		if slice1[i] != slice2[i] {
-			return false
-		}
-	}
-
-	// If the loop completes without returning false, return true
-	return true
-}
-
-func matcher(path []string, matchPath []string) bool {
-	if isSubSlice(path, matchPath) {
-		return true
-	}
-	return false
-}
-
-func printJSON(data map[string]interface{}, indent string, matchPath []string) {
-	fmt.Println(indent, " {")
-	currentPath := []string{}
-	printJSONHelper(data, indent+"    ", currentPath, matchPath, matcher)
-	fmt.Println(indent, " }")
-}
-
-func printJsonMain() {
-	jsonData := queryContent
-
-	var data map[string]interface{}
-	err := json.Unmarshal([]byte(jsonData), &data)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	fmt.Println("JSON Content:")
-	matchPath := []string{"query", "bool", "must", "*", "term"}
-
-	printJSON(data, "", matchPath)
 }
 
 func traverse() {
